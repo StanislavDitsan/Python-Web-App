1f7655f2e7382a5f730c1dc5a07f43f342a1e0ad
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-09-05 13:54:14 +0000

    Importing the Flask class object from the flask library

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Importing the Flask class object from the flask library.
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Home Page"
+
+
+@app.route('/About/')
+def about():
+    return "About content ...!"
+
+
+# If name equals to main, then run the app,
+if __name__ == "__main__":
+    app.run(debug=True)
